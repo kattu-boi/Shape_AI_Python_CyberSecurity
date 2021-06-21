0b3508fdd15314820650122ff218f89233decc83
kattu-boi <85389314+kattu-boi@users.noreply.github.com>
2021-06-22 03:07:01 +0530

    Delete weather_requests.py

--- weather_requests.py
+++
@@ -1,5 +0,0 @@
-import requests
-r = requests.get('https://github.com/kattu-boi/Shape_AI_Python_CyberSecurity/blob/main/weather.py')
-
-with open('weather_requests.txt','wb') as fd:
-    fd.write(r.content)
